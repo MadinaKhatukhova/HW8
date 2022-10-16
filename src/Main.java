46c12051b319e8d2f0c512f548b06a5b7ae99dfb
Madina <madina-khat@yandex.ru>
2022-10-16 16:39:45 +0300

    Задание 3_1

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,17 @@
 public class Main {
     public static void main(String[] args) {
 
-        int friday = 3;
-        for (int i = 1; i <= 31; i++) {
-            if (i == friday) {
-                System.out.println("Сегодня пятница, " + friday + "-е число. Необходимо подготовить отчет");
-                friday += 7;
+        int year = 0;
+        int periodCometPassage = 79;
+        int nowYear = 2022;
+        int last200Years = nowYear - 200;
+        int next100Years = nowYear + 100;
+        while (year < next100Years) {
+            if (year > last200Years) {
+                System.out.println(year);
+            }
+            year += periodCometPassage;
+        }
 
 
-}}}}
+}}
