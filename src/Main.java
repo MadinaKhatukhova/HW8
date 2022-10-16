abb15ccb0427fd98f0158f8017bccd45469c1b7b
Madina <madina-khat@yandex.ru>
2022-10-16 16:14:31 +0300

    Задание 1_3

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,13 @@
 public class Main {
     public static void main(String[] args) {
 
-        int num = 0;
-        while (num < 10) {
-            num++;
-            System.out.print(num + " ");
-        }
-        System.out.println();
-        for (;num > 0; --num) {
-            System.out.print(num + " ");
+        int countryPopulation = 12_000_000;
+        int birthRate;
+        int mortalityRate;
+        for (int i = 1; i < 11; i++) {
+            birthRate = 17 * countryPopulation / 1000;
+            mortalityRate = 8 * countryPopulation / 1000;
+            countryPopulation += birthRate - mortalityRate;
+            System.out.println("Год " + i + ", численность населения составляет " + countryPopulation);
 
 }}}
