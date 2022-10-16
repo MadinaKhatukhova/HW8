fc5e7099f226314055ce0c3af8ade7245f1eeaeb
Madina <madina-khat@yandex.ru>
2022-10-16 16:10:02 +0300

    Задание 1_1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
+        int salary = 15_000;
+        int total = 0;
+        int month = 0;
+        int goal = 2_459_000;
+        while (total <= goal) {
+            month++;
+            total += salary + total / 100;
+            System.out.println("Месяц " + month + ", сумма накоплений равна " + total + " рублей.");
+        }
+
+}}
