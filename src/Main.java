9c73ba67079622bbb06fd8002389dca8618f016c
Madina <madina-khat@yandex.ru>
2022-10-16 16:41:43 +0300

    Задание 3_2

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,11 @@
 public class Main {
     public static void main(String[] args) {
 
-        int year = 0;
-        int periodCometPassage = 79;
-        int nowYear = 2022;
-        int last200Years = nowYear - 200;
-        int next100Years = nowYear + 100;
-        while (year < next100Years) {
-            if (year > last200Years) {
-                System.out.println(year);
-            }
-            year += periodCometPassage;
+        int num2 = 2;
+        int multiplication;
+        for (int i = 1; i <= 10; i++) {
+            multiplication = num2 * i;
+            System.out.println(num2 + "*" + i + "=" + multiplication);
         }
 
 
